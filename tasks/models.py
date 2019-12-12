33bd99f7ed03e61591d43dbdb95317db3f6a63f8
Kohei Shinkawa <kohei.shinkawa@asterist.com>
2019-12-12 11:22:01 +0900

    UserRecipeとUserTaskを追加

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-
-from django.db import models
 from rest_framework import serializers
+from users.models import User
 
 # Create your models here.
 class Task(models.Model):
@@ -26,3 +25,17 @@ class Setting(models.Model):
     class Meta:
             ordering = ('id', )
             db_table = "settings"
+
+class UserTask(models.Model):
+    id =  models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    disc = models.TextField(max_length=100, null=True)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    task = models.ForeignKey(Task, on_delete=models.DO_NOTHING)
+    data = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+            ordering = ('id', )
+            db_table = "user_tasks"
