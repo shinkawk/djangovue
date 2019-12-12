48a036aff5b5e7d7f09b95cbb71de0ce0e1dd3f5
Kohei Shinkawa <kohei.shinkawa@asterist.com>
2019-12-12 11:09:12 +0900

    RicipeAdminを追加

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display=  ('name', 'disc', 'task1', 'task2', 'task3', 'task4',
+    'task5', 'task6', 'task7', 'task8', 'task9', 'task10', 
+    'created_at', 'updated_at')
+    fields = ('name', 'disc', 'task1', 'task2', 'task3', 'task4',
+    'task5', 'task6', 'task7', 'task8', 'task9', 'task10')
+
+    class Meta:
+        ordering = ('created_at')
