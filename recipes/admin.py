800ac8131eb00dcb275550b0353a2bbfc422c0b8
Kohei Shinkawa <kohei.shinkawa@asterist.com>
2019-12-13 11:27:42 +0900

    AdminにUserRecipeを追加

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 
-from .models import Recipe
+from .models import Recipe, UserRecipe
 # Register your models here.
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display=  ('name', 'disc', 'task1', 'task2', 'task3', 'task4',
+    'task5', 'task6', 'task7', 'task8', 'task9', 'task10', 
+    'created_at', 'updated_at')
+    fields = ('name', 'disc', 'task1', 'task2', 'task3', 'task4',
+    'task5', 'task6', 'task7', 'task8', 'task9', 'task10')
+
+    class Meta:
+        ordering = ('created_at')
+
+@admin.register(UserRecipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display=  ('name', 'disc', 'user','task1', 'task2', 'task3', 'task4',
     'task5', 'task6', 'task7', 'task8', 'task9', 'task10', 
     'created_at', 'updated_at')
     fields = ('name', 'disc', 'task1', 'task2', 'task3', 'task4',
