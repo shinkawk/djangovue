796f066fc8433fdb012f72ed5a30cb67eafba190
shinkawk <shinkawk@gmail.com>
2019-12-15 15:01:09 +0900

    Profile APIのテストを実装

--- users/factory.py
+++ users/factory.py
@@ -1,29 +1,30 @@
-from users.models import User
+from users.models import User, UserResource
 from factory import DjangoModelFactory, lazy_attribute
 from factory.fuzzy import FuzzyText, FuzzyInteger, FuzzyAttribute
 from django.contrib.auth.models import User as auth0User
 from social_django.models import UserSocialAuth
 import datetime
 from django.utils import timezone
+from faker import Factory
+
+faker = Factory.create()
 
 class UserModelFactory(DjangoModelFactory):
     class Meta:
         model = User
-    id = FuzzyInteger(1, 100)
-    name = FuzzyText()
-    uid = FuzzyText()
-    email = "test@mail.com"
-    pic = FuzzyText()
+    name = faker.name()
+    uid = faker.uuid4()
+    email = faker.email()
+    pic = faker.file_path()
     
 class Auth0UserModelFactory(DjangoModelFactory):
     class Meta:
         model = auth0User
-    id = FuzzyInteger(1, 100)
-    password = FuzzyText()
-    first_name = FuzzyText()
-    last_name = FuzzyText()
-    username = FuzzyText()
-    email = "test@mail.com"
+    password = faker.password()
+    first_name = faker.first_name()
+    last_name = faker.last_name()
+    username = faker.user_name()
+    email = faker.email()
     last_login = timezone.now()
     date_joined = timezone.now()
     is_superuser = 0
@@ -33,11 +34,17 @@ class Auth0UserModelFactory(DjangoModelFactory):
 class UserSocialAuthModelFactory(DjangoModelFactory):
     class Meta:
         model = UserSocialAuth
-    id = FuzzyInteger(1, 100)
     provider = "auth0"
-    uid = FuzzyText()
+    uid = faker.uuid4()
     user_id = FuzzyInteger(1, 100)
     extra_data = FuzzyText()
+
+class UserResorceModelFactory(DjangoModelFactory):
+    class Meta:
+        model = UserResource
+    path = faker.file_path()
+    format = "json/text"
+    user = 1
 
 def createUserInfo():
     user = UserModelFactory.create()
--- users/tests.py
+++ users/tests.py
@@ -4,14 +4,48 @@ from users.factory import *
 from users.views import UserAPIView
 
 class ProfileTest(TestCase):
-
     def setUp(self):
-        self.user, self.auth0User, self.social_auth = createUserInfo()
         self.client = APIClient()
-        self.auth0User.set_password("abc")
+        self.user, self.auth0User, self.social_auth = createUserInfo()
+        self.resources = UserResorceModelFactory.create_batch(10, user=self.user)
+        self.password = "hogehoge"
+        self.auth0User.set_password(self.password)
         self.auth0User.save()
-        #self.client.login(username= self.auth0User.username, password = "abc")
 
-    def testGet(self):
+    def test_get(self):
         response = self.client.get('/profile/')
-        print(response)
+        self.assertEqual(response.status_code, 403)
+
+        self.client.login(username= self.auth0User.username, password = self.password)
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['id'], self.user.id)
+        self.assertEqual(response.data['name'], self.user.name)
+        self.assertEqual(response.data['uid'], self.user.uid)
+        self.assertEqual(response.data['email'], self.user.email)
+        self.assertEqual(response.data['pic'], self.user.pic)
+        self.assertEqual(len(response.data['resources']), 10)
+
+    def test_put(self):
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 403)
+
+        self.client.login(username= self.auth0User.username, password = self.password)
+        response = self.client.put('/profile/', {'name': 'user1', 'email': 'test@mail.com', 'pic':'dog.jpg'} )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.get(id=self.user.id).name, 'user1')
+        self.assertEqual(User.objects.get(id=self.user.id).email, 'test@mail.com')
+        self.assertEqual(User.objects.get(id=self.user.id).pic, 'dog.jpg')
+
+        response = self.client.put('/profile/', {'id': 5 ,'name': 'user1', 'uid':'this should fail' ,'email': 'test@mail.com', 'pic':'dog.jpg'} )
+        self.assertEqual(User.objects.get(id=self.user.id).uid, self.user.uid)
+        self.assertEqual(User.objects.get(id=self.user.id).id, self.user.id)
+
+    def test_delete(self):
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 403)
+
+        self.client.login(username= self.auth0User.username, password = self.password)
+        response = self.client.delete('/profile/')
+        self.assertEqual(response.status_code, 204)
+        self.assertRaises(User.DoesNotExist, User.objects.get, id=self.user.id)
